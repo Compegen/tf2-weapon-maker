595e628626f3f5a64bdeb31e0611c98ebceaa3d9
Derek R <derek13f6@gmail.com>
2016-09-17 20:09:02 -0500

    Add files via upload

---
+++ site/attributeProcessing.js
@@ -0,0 +1,76 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// SETS TABLE VALUES ///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+	var percentage = "percentage", additive = "additive", absolute = "absolute";
+	
+	var i, o, c = ["percentageAttributes", "additiveAttributes", "absoluteAttributes"];
+	for (o in c)
+	{
+		console.log(c[o]+"List");
+		var currentElement = document.getElementById(c[o]+"List");
+		for(i in tfattributes[c[o]])			
+		{
+			var opt = document.createElement("option");
+			opt.value = [tfattributes[c[o]][i].state, tfattributes[c[o]][i].phyVal, tfattributes[c[o]][i].techVal];
+			
+			opt.innerHTML = "(" + tfattributes[c[o]][i].state + ") : " + tfattributes[c[o]][i].techVal;
+			currentElement.appendChild(opt);
+		}
+	}
+	
+function getWepName() {
+				var wepName = document.getElementById("weaponNameInput");
+				//console.log(wepName);
+				document.getElementById("weaponName").innerHTML = wepName.value;
+			}
+	
+function getAttribute(typeset) {
+	
+	//Make sure valid inputs are given
+	var tNum = typeset+"Number";
+	var tList = typeset+"AttributesList";
+	
+	if (typeset == "absolute") { entry = 1;} 
+	else { var entry = Number(document.getElementById(typeset+"Number").value) }
+	var fileDot = entry;
+	
+	//console.log(document.getElementById(tList));
+	if(document.getElementById(typeset+"AttributesList").value == "none"){ alert("No input given!"); return; }
+	if( isNaN(entry) || entry <= 0){ alert("Invalid value given!"); return;}
+	
+	var attrHolder = document.createElement("p");
+	var selected = document.getElementById(typeset+"AttributesList").value;
+	
+	selected = selected.split(","); //We want the stored_at, state, and phyVal all seperated
+	//console.log(selected);
+	
+	var techID = selected[2];
+
+	//FOR PERCENTAGE//
+	if(typeset == "percentage") { entry = (entry*100) + "%";}
+	//console.log(entry);
+	selected[1] = selected[1].replace("&s", entry);
+	if( selected[0] == '+') { 
+	
+		attrHolder.innerHTML = selected[1];
+		attrHolder.id = [fileDot, techID];
+		document.getElementById("positiveStats").appendChild(attrHolder);
+		
+	} else if( selected[0] == "-") { 
+
+		attrHolder.innerHTML = selected[1];
+		attrHolder.id = [fileDot, techID];
+		document.getElementById("negativeStats").appendChild(attrHolder);
+		
+	} else { 
+		
+		attrHolder.innerHTML = selected[1];
+		attrHolder.id = [fileDot, techID];
+		document.getElementById("neutralStats").appendChild(attrHolder);
+	}
+	return;
+}
+
+function generateOutput () {
+	
+}
---
+++ site/tfattributes.js
@@ -0,0 +1,296 @@
+var tfattributes = {
+  "percentageAttributes" : [
+    {
+      "stored_at" : 0,
+      "techVal" : "damage penalty",
+      "phyVal" : "&s damage penalty",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 1,
+      "techVal" : "damage bonus",
+      "phyVal" : "+&s damage bonus",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 2,
+      "techVal" : "clip size penalty",
+      "phyVal" : "&s clip size",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 3,
+      "techVal" : "clip size bonus",
+      "phyVal" : "+&s clip size",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 4,
+      "techVal" : "fire rate penalty",
+      "phyVal" : "&s slower firing speed",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 5,
+      "techVal" : "fire rate bonus",
+      "phyVal" : "+&s faster firing rate",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 6,
+      "techVal" : "heal rate penalty",
+      "phyVal" : "&s heal rate",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 7,
+      "techVal" : "heal rate bonus",
+      "phyVal" : "+&s heal rate",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 8,
+      "techVal" : "ubercharge rate penalty",
+      "phyVal" : "&s UberCharge rate",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 9,
+      "techVal" : "ubercharge rate bonus",
+      "phyVal" : "+&s UberCharge rate",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 10,
+      "techVal" : "overheal bonus",
+      "phyVal" : "+&s max overheal",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 11,
+      "techVal" : "overheal decay penalty",
+      "phyVal" : "&s shorter overheal time",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 12,
+      "techVal" : "overheal decay bonus",
+      "phyVal" : "+&s longer overheal time",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 13,
+      "techVal" : "dmg penalty vs nonburning",
+      "phyVal" : "&s damage vs non-burning players",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 14,
+      "techVal" : "mult cloak meter consume rate",
+      "phyVal" : "+&s cloak drain rate",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 15,
+      "techVal" : "mult cloak meter regen rate",
+      "phyVal" : "+&s regen rate",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 16,
+      "techVal" : "spread penalty",
+      "phyVal" : "&s less acurate",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 17,
+      "techVal" : "dmg penalty vs nonstunned",
+      "phyVal" : "&s damage vs non-stunned players",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 18,
+      "techVal" : "sniper charge per sec",
+      "phyVal" : "+&s charge rate",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 19,
+      "techVal" : "bullets per shot bonus",
+      "phyVal" : "+&s bullets per shot",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 20,
+      "techVal" : "sniper zoom penalty",
+      "phyVal" : "&s zoom reduction",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 21,
+      "techVal" : "absorb damage while cloaked",
+      "phyVal" : "Absorbs &s damage while cloaked",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 22,
+      "techVal" : "move speed penalty",
+      "phyVal" : "&s slower move speed on wearer",
+      "state" : "-"
+    }
+  ],
+  "additiveAttributes" : [
+    {
+      "stored_at" : 0,
+      "techVal" : "heal on hit for rapidfire",
+      "phyVal" : "On Hit: Gain up to +&s health",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 1,
+      "techVal" : "add uber charge on hit",
+      "phyVal" : "On Hit: &s% UberCharge added",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 2,
+      "techVal" : "tmp dmgbuff on hit",
+      "phyVal" : "On Hit: +&s% damage bonus",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 3,
+      "techVal" : "max health additive bonus",
+      "phyVal" : "+&s max health on wearer",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 4,
+      "techVal" : "critboost on kill",
+      "phyVal" : "On Kill: &s seconds of 100% critical chance",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 5,
+      "techVal" : "slow enemy on hit",
+      "phyVal" : "On Hit: &s% chance to slow target",
+      "state" : "+"
+    }
+  ],
+  "absoluteAttributes" : [
+    {
+      "stored_at" : 0,
+      "techVal" : "overheal decay disabled",
+      "phyVal" : "Overheal bonus doesn't decay",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 1,
+      "techVal" : "crit mod disabled",
+      "phyVal" : "No random critical hits",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 2,
+      "techVal" : "medigun charge is crit boost",
+      "phyVal" : "UberCharge grants 100% critical chance",
+      "state" : "="
+    },
+    {
+      "stored_at" : 3,
+      "techVal" : "crit vs burning players",
+      "phyVal" : "100% critical chance vs burning players",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 4,
+      "techVal" : "no crit vs nonburning",
+      "phyVal" : "No critical hits vs non-burning players",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 5,
+      "techVal" : "mod flamethrower push",
+      "phyVal" : "No compression blast",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 6,
+      "techVal" : "mod flamethrower back crit",
+      "phyVal" : "100% critical chance from behind",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 7,
+      "techVal" : "set cloak is feign death",
+      "phyVal" : "Cloak Type: Feign Death.",
+      "state" : "="
+    },
+    {
+      "stored_at" : 8,
+      "techVal" : "mod bat launches balls",
+      "phyVal" : "Alt-Fire: Launches a ball that stuns opponents",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 9,
+      "techVal" : "zoom speed mod disabled",
+      "phyVal" : "No move speed penalty from zoom",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 10,
+      "techVal" : "sniper no headshot",
+      "phyVal" : "No headshots",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 11,
+      "techVal" : "scattergun no reload single",
+      "phyVal" : "Scattergun reloads entire clip at once",
+      "state" : "="
+    },
+    {
+      "stored_at" : 12,
+      "techVal" : "scattergun has knockback",
+      "phyVal" : "Knockback on the targer and shooter",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 13,
+      "techVal" : "sniper no charge",
+      "phyVal" : "No zoom or damage charge",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 14,
+      "techVal" : "set cloak is movement based",
+      "phyVal" : "Cloak Type: Motion Sensitive",
+      "state" : "="
+    },
+    {
+      "stored_at" : 15,
+      "techVal" : "no double jump",
+      "phyVal" : "Disables double jump",
+      "state" : "-"
+    },
+    {
+      "stored_at" : 16,
+      "techVal" : "revolver use hit locations",
+      "phyVal" : "Crits on headshot",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 17,
+      "techVal" : "backstab shield",
+      "phyVal" : "Blocks a single backstab attempt",
+      "state" : "+"
+    },
+    {
+      "stored_at" : 18,
+      "techVal" : "fire retardant",
+      "phyVal" : "Wearer cannot ignite",
+      "state" : "+"
+    }
+  ]
+}
